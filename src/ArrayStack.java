94f6863911108aeba07f5c95ff2c6fc3a3e7f129
zlarbals <zlarbals@gmail.com>
2019-11-06 03:07:28 +0900

    ArrayStack

---
+++ src/ArrayStack.java
@@ -0,0 +1,51 @@
+public class ArrayStack {
+    private int top;
+    private int maxSize;
+    private Object[] stackArray;
+
+    //stack 생성
+    public ArrayStack(int maxSize) {
+        this.maxSize = maxSize;
+        this.stackArray = new Object[maxSize];
+        this.top = -1;
+    }
+
+    //stack data 입력.
+    public void Push(Object data){
+        if(isFull())
+            return;
+        stackArray[++top] = data;
+    }
+
+    //stack data 제거.
+    public Object Pop(){
+        if(isEmpty())
+            return 0;
+        return stackArray[top--];
+    }
+
+    //stack 가장 위 data 반환.
+    public Object Peek(){
+        if(isEmpty())
+            return 0;
+
+        return stackArray[top];
+    }
+
+
+    public boolean isEmpty(){
+        if(top==-1)
+            return true;
+        else
+            return false;
+
+    }
+
+    public boolean isFull(){
+        if(top == maxSize)
+            return true;
+        else
+            return false;
+    }
+}
+
